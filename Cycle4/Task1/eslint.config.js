cb93c7ab36247b5c2fd428d9de9576ed7636e1f6
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-02-06 00:41:32 +0200

    Fix eslint.config.js and test the rules

--- Cycle4/Task1/eslint.config.js
+++ Cycle4/Task1/eslint.config.js
@@ -1,54 +1,50 @@
-import js from '@eslint/js';
 import checkFile from 'eslint-plugin-check-file';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-   { ignores: ['dist'] },
-   {
-      extends: [
-         js.configs.recommended,
-         ...tseslint.configs.recommended,
-         'prettier',
+export default {
+   ignores: ['dist'],
+   files: ['**/*.{ts,tsx}'],
+   languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+         projectService: true,
+      },
+   },
+   plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'check-file': checkFile,
+   },
+   rules: {
+      // custom rules
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'error',
+      quotes: ['error', 'single'],
+      'check-file/filename-naming-convention': [
+         'error',
+         {
+            '**/*.{ts,tsx}': 'KEBAB_CASE',
+         },
+         {
+            ignoreMiddleExtensions: true,
+         },
       ],
-      files: ['**/*.{ts,tsx}'],
-      languageOptions: {
-         ecmaVersion: 2020,
-         globals: globals.browser,
-      },
-      plugins: {
-         'react-hooks': reactHooks,
-         'react-refresh': reactRefresh,
-         'check-file': checkFile,
-      },
-      rules: {
-         ...reactHooks.configs.recommended.rules,
-         'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-         ],
-
-         // custom rules
-         'prefer-arrow-callback': ['error'],
-         'prefer-template': ['error'],
-         quotes: ['error', 'single'],
-         'check-file/filename-naming-convention': [
-            'error',
-            {
-               '**/*.{ts,tsx}': 'KEBAB_CASE',
-            },
-            {
-               ignoreMiddleExtensions: true,
-            },
-         ],
-         'check-file/folder-naming-convention': [
-            'error',
-            {
-               'src/**': 'KEBAB_CASE',
-            },
-         ],
-      },
-   }
-);
+      'check-file/folder-naming-convention': [
+         'error',
+         {
+            'src/**': 'KEBAB_CASE',
+         },
+      ],
+      'react-refresh/only-export-components': [
+         'warn',
+         { allowConstantExport: true },
+      ],
+      ...reactHooks.configs.recommended.rules,
+   },
+};
--- Cycle4/Task1/src/App.tsx
+++ Cycle4/Task1/src/App.tsx
@@ -1,13 +1,20 @@
 import { Button } from '@/components/ui/button';
 
 function App() {
-   [].map(function (item) {
+   [].map((item) => {
       console.log(item);
+      let a = 'a';
+      let b = 'b';
+
+      let c = `${a} ${b}`;
+      console.log(c);
    });
    return (
       <>
-         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-         <Button>Click me</Button>
+         <div className="flex h-screen flex-col items-center justify-center gap-4">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <Button>Click me</Button>
+         </div>
       </>
    );
 }
