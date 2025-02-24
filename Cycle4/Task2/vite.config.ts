3b175a4742074f81c932bcbcd86f1c0bcb6f206d
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-02-24 23:53:14 +0200

    Add react compiler

--- Cycle4/Task2/vite.config.ts
+++ Cycle4/Task2/vite.config.ts
@@ -1,10 +1,15 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+   plugins: [
+      react({
+         babel: {
+            plugins: [['babel-plugin-react-compiler']],
+         },
+      }),
+   ],
 
    server: {
       port: 7000,
@@ -12,7 +17,7 @@ export default defineConfig({
 
    resolve: {
       alias: {
-         "@": path.resolve(__dirname, "./src"),
+         '@': path.resolve(__dirname, './src'),
       },
    },
 });
