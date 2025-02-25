89b8e43651a7a06a5d25d201f03515be6e4d0212
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-02-25 17:59:53 +0200

    Format the files

--- Cycle4/Task2/src/data/mock-weather.ts
+++ Cycle4/Task2/src/data/mock-weather.ts
@@ -1,22 +1,22 @@
-import { WeatherData } from "@/types/weather-types";
+import { WeatherData } from '@/types/weather-types';
 
 export const weatherData: WeatherData = {
-   city: "Porto Alegre",
-   state: "RS",
+   city: 'Porto Alegre',
+   state: 'RS',
    currentTemp: 28,
    minTemp: 26,
    maxTemp: 32,
-   condition: "Partly cloudy",
+   condition: 'Partly cloudy',
    thermalSensation: 26,
    rainProbability: 0,
    windSpeed: 8,
    humidity: 40,
    uvIndex: 5,
    forecast: [
-      { day: "Tomorrow", temp: 32, minTemp: 26, condition: "Storm" },
-      { day: "Wednesday", temp: 32, minTemp: 26, condition: "Rain" },
-      { day: "Thursday", temp: 32, minTemp: 26, condition: "Partly cloudy" },
-      { day: "Friday", temp: 32, minTemp: 26, condition: "Cloudy" },
-      { day: "Saturday", temp: 32, minTemp: 26, condition: "Sunny" },
+      { day: 'Tomorrow', temp: 32, minTemp: 26, condition: 'Storm' },
+      { day: 'Wednesday', temp: 32, minTemp: 26, condition: 'Rain' },
+      { day: 'Thursday', temp: 32, minTemp: 26, condition: 'Partly cloudy' },
+      { day: 'Friday', temp: 32, minTemp: 26, condition: 'Cloudy' },
+      { day: 'Saturday', temp: 32, minTemp: 26, condition: 'Sunny' },
    ],
 };
