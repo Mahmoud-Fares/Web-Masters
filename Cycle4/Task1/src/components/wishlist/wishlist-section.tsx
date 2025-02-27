3d18c0e071891534b663fc2405ad5b5ca58c78f0
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-02-27 23:39:45 +0200

    Add move all to cart functionality

--- Cycle4/Task1/src/components/wishlist/wishlist-section.tsx
+++ Cycle4/Task1/src/components/wishlist/wishlist-section.tsx
@@ -1,11 +1,23 @@
+import { toast } from 'sonner';
+
 import Container from '@/components/container';
 import ProductList from '@/components/product/product-list';
 import { Button } from '@/components/ui/button';
 import { EmptyState } from '@/components/ui/empty-state';
+import { useCartStore } from '@/lib/stores/cart-store';
 import { useWishlistStore } from '@/lib/stores/wishlist-store';
 
 export default function WishlistSection() {
-   const { items: products } = useWishlistStore();
+   const { items: products, toggleItem } = useWishlistStore();
+   const { addItem } = useCartStore();
+
+   const moveAllToBag = () => {
+      products.forEach((product) => {
+         addItem({ product });
+         toggleItem(product);
+      });
+      toast.success('All items moved to cart!');
+   };
 
    if (products.length === 0)
       return (
@@ -25,7 +37,11 @@ export default function WishlistSection() {
             <div className="flex items-center justify-between">
                <h2 className="font-medium">Wishlist ({products.length})</h2>
 
-               <Button variant={'outline'} className="capitalize">
+               <Button
+                  variant={'outline'}
+                  className="capitalize"
+                  onClick={moveAllToBag}
+               >
                   Move all to bag
                </Button>
             </div>
