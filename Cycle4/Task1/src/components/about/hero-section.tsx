6111679a93711571f4d1e2aa9a70b5d900475298
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-03-07 19:25:08 +0200

    Fix the about page layout
    make the about page perfectly match the UI design

--- Cycle4/Task1/src/components/about/hero-section.tsx
+++ Cycle4/Task1/src/components/about/hero-section.tsx
@@ -1,25 +1,31 @@
+import { cn } from '@/lib/utils';
+
 import Container from '../container';
 
 export default function HeroSection() {
    return (
-      // todo make this my-section and fix the layout chaos
-      <section className="relative py-section">
-         <Container className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
-            <HeroSectionContent />
+      // two grids the image is outside the container and the details are inside the container
+      <section className="relative pb-section *:grid *:grid-cols-1 *:items-center *:gap-8 *:lg:grid-cols-2">
+         {/* in small screens one columns grid with the details */}
+         {/* in large screens two columns grid with the details and an empty div to put the details on the left inside the container */}
+         <Container className="inset-0 pt-section lg:absolute lg:pt-0">
+            <HeroSectionDetails />
 
-            <div className="right-0 top-1/2 aspect-[7/6] overflow-hidden rounded-l md:absolute md:h-full md:max-w-[50%] md:-translate-y-1/2 xl:relative xl:max-h-[700px] xl:max-w-full xl:rounded">
-               <img
-                  src="/images/about-side-image.png"
-                  alt="Happy shoppers with shopping bags"
-                  className="h-full w-full object-cover object-center"
-               />
-            </div>
+            <div />
          </Container>
+
+         {/* in small screens one column grid with the image */}
+         {/* in large screens two columns grid with the image and an empty div to put the image on the right outside the container */}
+         <div>
+            <div className="hidden lg:block" />
+
+            <HeroSectionImage />
+         </div>
       </section>
    );
 }
 
-const HeroSectionContent = () => {
+const HeroSectionDetails = () => {
    return (
       <div className="flex flex-col justify-center gap-6 px-4">
          <h1 className="text-4xl font-semibold md:text-5xl">Our Story</h1>
@@ -41,3 +47,21 @@ const HeroSectionContent = () => {
       </div>
    );
 };
+
+const HeroSectionImage = () => (
+   <figure
+      className={cn(
+         'flex max-w-screen-lg place-content-center overflow-hidden p-4',
+         'lg:aspect-[7/6] lg:justify-end lg:p-0'
+      )}
+   >
+      <img
+         src="/images/about-side-image.png"
+         alt="Happy shoppers with shopping bags"
+         className={cn(
+            'block rounded object-cover object-center',
+            'lg:rounded-r-none [@media(min-width:2080px)]:rounded-r'
+         )}
+      />
+   </figure>
+);
